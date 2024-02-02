53633f4f891d7c3400f631937aea0c79d82dbbc6
Aswinraaj23 <aswin@testpress.in>
2024-02-02 20:24:42 +0530

    Add List and create view for Stimulus app

--- hotwire_django_app/stimulus_basic/urls.py
+++ hotwire_django_app/stimulus_basic/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import counter_view
+from .views import counter_view, create_view, list_view 
 
 app_name = 'stimulus-basic'
 
 urlpatterns = [
     path('counter/', counter_view, name='counter'),
+    # path('turbo_frame_load/', turbo_frame_load_view, name='turbo_frame_load'),
+    path('list/', list_view, name='task-list'),                   # new
+    path('create/', create_view, name='task-create'), 
 ]
--- hotwire_django_app/stimulus_basic/views.py
+++ hotwire_django_app/stimulus_basic/views.py
@@ -1,4 +1,36 @@
+import http
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from hotwire_django_app.tasks.forms import TaskForm
+from hotwire_django_app.tasks.models import Task
 from django.shortcuts import render
 
 def counter_view(request):
     return render(request, 'stimulus_basic/counter.html')
+
+def create_view(request):                         # new
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'Task created successfully')
+            return redirect(reverse('stimulus-basic:task-list'))
+
+        status = http.HTTPStatus.UNPROCESSABLE_ENTITY
+    else:
+        status = http.HTTPStatus.OK
+        form = TaskForm()
+
+    return render(request, 'stimulus_basic/create_page.html', {'form': form}, status=status)
+
+
+def list_view(request):                           # new
+    object_list = Task.objects.all().order_by('-pk')
+
+    context = {
+        "object_list": object_list,
+    }
+
+    return render(request, 'stimulus_basic/list_page.html', context)
